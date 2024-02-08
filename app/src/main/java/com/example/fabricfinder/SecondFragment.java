bdceea518c0aef36cfd4d274314e41078ca59526
LadyMinks <minka-96@outlook.com>
2024-02-08 17:31:32 +0100

    lengthSpinner and widthSpinner now use string resources.

--- app/src/main/java/com/example/fabricfinder/SecondFragment.java
+++ app/src/main/java/com/example/fabricfinder/SecondFragment.java
@@ -4,12 +4,14 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import com.example.fabricfinder.databinding.FragmentSecondBinding;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
-
-import com.example.fabricfinder.databinding.FragmentSecondBinding;
 
 public class SecondFragment extends Fragment {
 
@@ -28,6 +30,17 @@ public class SecondFragment extends Fragment {
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        Spinner lengthSpinner = (Spinner) view.findViewById(R.id.spinner_length_input);
+        Spinner widthSpinner = (Spinner) view.findViewById(R.id.spinner_width_input);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+                this.requireContext(),
+                R.array.measurements_array,
+                android.R.layout.simple_spinner_item
+        );
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        lengthSpinner.setAdapter(adapter);
+        widthSpinner.setAdapter(adapter);
 
         binding.buttonSecond.setOnClickListener(new View.OnClickListener() {
             @Override
